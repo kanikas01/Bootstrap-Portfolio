762fb1b6e10e51f5aa103746ab71e166f3c58758
kanikas01 <larvakurt@yahoo.com>
2018-12-11 12:59:06 -0800

    Put navbar and footer in separate js files to cut down on redundancy

---
+++ assets/javascript/footer.js
@@ -0,0 +1,6 @@
+document.write(`
+  <footer class="footer">
+    <div class="container">
+      <span class="text-muted">Copyright &copy;</span>
+    </div>
+  </footer>`);
---
+++ assets/javascript/navbar.js
@@ -0,0 +1,23 @@
+document.write(`
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand col-9" href="index.html">
+      <h1>Devin Willoughby</h1>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav"
+      aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav col-3">
+        <li class="nav-item">
+          <a class="nav-link" href="index.html">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="portfolio.html">Portfolio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contact.html">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </nav>`);
